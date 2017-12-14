4690e7888f2a63095a1f18953be287f4e864a888
Martyn Janes <martyn@obany.com>
2017-12-14 16:23:32 +0300

    Added E2E Tests

--- create-apps.sh
+++ create-apps.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=vue-javascript --title="Vue JavaScript" --profile=VueJavaScript --outputDirectory=./apps/vue-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=vue-javascript --title="Vue JavaScript" --profile=VueJavaScript --unitTestFramework=Jasmine --e2eTestFramework=Jasmine --outputDirectory=./apps/vue-javascript
+mkdir -p ./apps/vue-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/vue-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vue-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vue-javascript
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=vue-typescript-amd --title="Vue TypeScript CommonJS" --profile=VueTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/vue-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=vue-typescript-commonjs --title="Vue TypeScript CommonJS" --profile=VueTypeScript --moduleType=CommonJS --bundler=Webpack --unitTestFramework=Jasmine --e2eTestFramework=Jasmine --outputDirectory=./apps/vue-typescript-commonjs
+mkdir -p ./apps/vue-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
+mkdir -p ./apps/vue-typescript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vue-typescript-commonjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vue-typescript-commonjs
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=vue-typescript-systemjs --title="Vue TypeScript SystemJS" --profile=VueTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/vue-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=vue-typescript-systemjs --title="Vue TypeScript SystemJS" --profile=VueTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestFramework=Jasmine --e2eTestFramework=Jasmine --outputDirectory=./apps/vue-typescript-systemjs
+mkdir -p ./apps/vue-typescript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
+mkdir -p ./apps/vue-typescript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/vue-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/vue-typescript-systemjs
